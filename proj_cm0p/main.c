8cd558ecc40d91c50526642f1b8918f81d11c160
Eliah Lohr <lohr.eliah@gmail.com>
2025-05-17 16:13:12 +0200

    GPIO pin proof on working

--- proj_cm0p/main.c
+++ proj_cm0p/main.c
@@ -75,13 +75,7 @@ int main(void)
 
     for (;;)
     {
-        Cy_SysLib_DelayUs(25);
-        Cy_GPIO_Write(CYBSP_PIEZO_0_PORT, CYBSP_PIEZO_0_PIN, 1UL);
-        Cy_GPIO_Write(CYBSP_PIEZO_1_PORT, CYBSP_PIEZO_1_PIN, 0UL);
-
-        Cy_SysLib_DelayUs(25);
-        Cy_GPIO_Write(CYBSP_PIEZO_0_PORT, CYBSP_PIEZO_0_PIN, 0UL);
-        Cy_GPIO_Write(CYBSP_PIEZO_1_PORT, CYBSP_PIEZO_1_PIN, 1UL);
+        Cy_SysPm_DeepSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
     }
 }
 
