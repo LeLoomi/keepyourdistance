52b35e9a9d4d117a5ba165eeb5d3bebc17264de6
KSKNico <nifrabe@outlook.de>
2025-06-30 19:32:26 +0200

    removed comments

--- proj_cm4/main.c
+++ proj_cm4/main.c
@@ -1,45 +1,3 @@
-/******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the PDM PCM Example
-*              for ModusToolbox.
-*
-* Related Document: See README.md
-*
-*
-*******************************************************************************
-* Copyright 2021-2022, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
-
 #include "cyhal.h"
 #include "cybsp.h"
 #include "cy_retarget_io.h"
@@ -431,25 +389,6 @@ static inline float32_t calculate_distance(float32_t time) {
     return (time * SPEED_OF_SOUND) / 2;
 }
 
-/*******************************************************************************
-* Function Name: main
-********************************************************************************
-* Summary:
-* The main function for Cortex-M4 CPU does the following:
-*  Initialization:
-*  - Initializes all the hardware blocks
-*  Do forever loop:
-*  - Check if PDM/PCM flag is set. If yes, report the current volume
-*  - Update the LED status based on the volume and the noise threshold
-*  - Check if the User Button was pressed. If yes, reset the noise threshold
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
 int main(void)
 {
     cy_rslt_t result;
